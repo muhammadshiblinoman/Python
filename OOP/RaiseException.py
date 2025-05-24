368e8f7cab494036d10cb0660f90b5bdc6224c20
Noman <muhammadshibli508@gmail.com>
2025-05-24 21:56:48 +0600

    new file add

--- OOP/RaiseException.py
+++ OOP/RaiseException.py
@@ -1,10 +1,29 @@
-class Accident(Exception):
-    def __init__(self, massage):
-        self.massage = massage
-    def handle(self):
-        print("accident occured, take detour")
+class AdultException(Exception):
+    pass
 
-try:
-    raise Accident('crash between two cars')
-except Accident as e:
-    e.handle()
+
+class Person:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+    def get_minor_age(self):
+        if int(self.age) >= 18:
+            raise AdultException
+        else:
+            return self.age
+
+    def display(self):
+        try:
+            print(f"age -> {self.get_minor_age()}")
+        except AdultException:
+            print("Person is an adult")
+        finally:
+            print(f"name -> {self.name}")
+
+
+# No exception
+Person("Bhavin", 17).display()
+
+# AdultException is raised
+Person("Dhaval", 23).display()
